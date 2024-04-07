dc801e2937e93a154dd04ad9bf59cee2f81e6b34
Chelseo Miguel <leonelmiguel1612@gmail.com>
2024-04-07 20:31:41 +0100

    update

---
+++ frontend/src/ContactList.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const ContactList = ({contacts}) => {
+    return <div>
+        <h2>Contacts</h2>
+        <table>
+            <thead>
+                <tr>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Email</th>
+                </tr>
+            </thead>
+            <tbody></tbody>
+        </table>
+    </div>
+}
